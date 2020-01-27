a27e4d858e46a4195e597859dce164788dc3c5f8
Saracoum <58824093+Saracoum@users.noreply.github.com>
2020-01-27 20:42:05 +0100

    mon commit sublissime

---
+++ Assets/Boids/FlockAgent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class FlockAgent : MonoBehaviour
+{
+
+    Collider2D agentcollider;
+    public Collider2D AgentCollider { 
+        get { 
+            return agentcollider; 
+        } 
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentcollider = GetComponent<Collider2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

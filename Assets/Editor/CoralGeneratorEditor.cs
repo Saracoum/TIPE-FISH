3a3179b23d91f10a00606708115923c6b5654821
mgodineau <mathieu.godineau.35@gmail.com>
2020-02-25 18:36:51 +0100

    ajout de corail1

---
+++ Assets/Editor/CoralGeneratorEditor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(CoralGenerator))]
+public class CoralGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI() {
+        
+    }
+}

8842c686bf0282d094da591f9c8b06bf88c28ace
mgodineau <mathieu.godineau.35@gmail.com>
2020-03-03 17:21:00 +0100

    magouilles avec des polynomes

---
+++ Assets/peuplement/Curve.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Curve
+{
+    
+    float Get(float x);
+    
+    void AddKey(float x, float y);
+    
+    List<Vector2> GetKeyList();
+    
+    void RemoveKey(int keyId);
+    
+}
---
+++ Assets/peuplement/LinearCurve.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearCurve : Curve
+{
+    
+    private SortedList<float, float> keyLst;
+    
+    public float Get(float x) {
+        if ( keyLst.Count == 0 ) {
+            return 0;
+        }
+        
+        int i = 0;
+        while( i<keyLst.Count-1 && keyLst.Keys[i+1] < x ) {
+            i++;
+        }
+        float a = (keyLst.Values[i+1] - keyLst.Values[i] ) / (keyLst.Keys[i+1] - keyLst.Keys[i]);
+        float b = keyLst.Values[i] - a * keyLst.Keys[i];
+        return a*x + b;
+    }
+    
+    public void AddKey(float x, float y){
+        if ( !keyLst.ContainsKey(x) ) {
+            keyLst.Remove(x);
+        }
+        keyLst.Add(x, y);
+    }
+    
+    public List<Vector2> GetKeyList(){
+        List<Vector2> lst = new List<Vector2>();
+        foreach( float x in keyLst.Keys ) {
+            lst.Add( new Vector2(x, keyLst[x]) );
+        }
+        return lst;
+    }
+    
+    public void RemoveKey(int keyId){
+        if ( keyId < keyLst.Count && keyId >= 0 ) {
+            keyLst.RemoveAt(keyId);
+        }
+    }
+    
+    public LinearCurve() {
+        keyLst = new SortedList<float, float>();
+    }
+    
+}

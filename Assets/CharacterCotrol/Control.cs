ea739ac96a7f6ddb1caf7c763a89c615cf68512d
Saracoum <58824093+Saracoum@users.noreply.github.com>
2020-03-26 21:12:41 +0100

    ajout player+camera

---
+++ Assets/CharacterCotrol/Control.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    CharacterController characterController;
+
+    [Range(0f, 10f)]
+    public float speed = 1.0f;
+    [Range(0f, 360f)]
+    public float rotSpeed = 90.0f; // rotate at 90 degrees/second
+
+    private Vector3 moveDirection = Vector3.zero;
+    
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime, 0, relativeTo:Space.World);
+        transform.Rotate(Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime, 0, 0, relativeTo: Space.Self);
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+    }
+}
---
+++ Assets/CharacterCotrol/playerFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerFollow : MonoBehaviour {
+
+    public Transform PlayerTransform;
+
+    private Vector3 camOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFacteur = 0.5f;
+
+    void Start()
+    {
+        camOffset = transform.position - PlayerTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPos = PlayerTransform.position + camOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFacteur);
+    }
+}
